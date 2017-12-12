a5029ceeaf98d11769c25c1166836a0c6f3ddbf0
MissZwei <1041090303@qq.com>
2017-12-12 16:54:48 +0800

    我的足迹

--- js/footprint.js
+++ js/footprint.js
@@ -1,14 +1,102 @@
 $(function(){
 	$(".bxg-delete").click(function(){
-		$(".bxg-toggle").show();
+		$(".bxg-toggle,.bxg-deleteAjax,.bxg-checkbox").show();
 		$(".bxg-normal").hide();
-		$(".bxg-checkbox").show();
-		$(".bxg-footprint>div").css('text-indent','30px');
+		$(".bxg-footprint>div").css('text-indent','0.84rem');
+		
 	});
 	$(".cancelSelect").click(function(){
-		$(".bxg-toggle").hide();
+		$(".bxg-toggle,.bxg-checkbox,.bxg-deleteAjax").hide();
 		$(".bxg-normal").show();
-		$(".bxg-checkbox").hide();
-		$(".bxg-footprint>div").css('text-indent','15px');
+		$(".bxg-footprint>div").css('text-indent','0.42rem');
+	});
+	//日期选择
+	$(".list-check label").click(function(){
+		var _this = $(this);
+		var inp = _this.next();
+		var parent = $(this).parents('ul');
+		var daySelect = parent.prev();
+		if(inp.prop('checked')){
+			_this.removeClass('checkActive');
+			if(!judgeSel(parent)){
+				daySelect.find('.day-check label').removeClass('checkActive');
+				daySelect.find('input').prop('checked',false);
+			}
+		}else{
+			_this.addClass('checkActive');
+			if(judgeSel(parent)){
+				console.log(2);
+				daySelect.find('.day-check label').addClass('checkActive');
+				daySelect.find('input').prop('checked',true);
+			}
+		}
+	});
+	//选择日期，判断日期下的子数量 obj
+	function judgeSel(obj){
+		var num = obj.find('li').length-1;
+		var selNum = obj.find('li').find('input:checked').length;
+		console.log("num:"+num+'-----   selNum:'+selNum);
+		if(num == selNum){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	//每天的选择
+	$(".day-check label").click(function(){
+		var _this = $(this);
+		var inp = $(this).next();
+		var listArr = $(this).parents('.daybtn').next();
+		if(inp.prop('checked')){
+			_this.removeClass('checkActive');
+			listArr.find('input').each(function(index,item){
+				if($(this).prop('checked')){
+					$(this).prop('checked',false);
+					$(this).prev().removeClass('checkActive');
+				}
+			});
+		}else{
+			_this.addClass('checkActive');
+			//循环检查
+			listArr.find('input').each(function(index,item){
+				if(!$(this).prop('checked')){
+					$(this).prop('checked',true);
+					$(this).prev().addClass('checkActive');
+				}
+			});	
+		}
+	});
+	//全选
+	$(".selectAll").click(function(){
+		var flag = Number($(this).attr("data-selAll"));
+		if(!flag){
+			$(this).html("取消全选");
+			$(".day-check").each(function(index,item){
+				var label = $(this).find("label");
+				var inp = $(this).find("input");
+				if(!inp.prop('checked')){
+					label.click();
+				}
+			});
+			$(this).attr("data-selAll",1);
+		}else{
+			$(this).html("全选");
+			$(".day-check").each(function(index,item){
+				var label = $(this).find("label");
+				var inp = $(this).find("input");
+				if(inp.prop('checked')){
+					label.click();
+				}
+			});
+			$(this).attr("data-selAll",0);
+		}
+	});
+	$(".bxg-deleteAjax").click(function(){
+		$(".day-check").each(function(index,item){
+			var dayInp = $(this).find('input');
+			if(dayInp.prop('checked')){
+				
+			}
+		});
 	});
 });
