b562541400b101e0dc741c8cf09d892248f677a2
MissZwei <1041090303@qq.com>
2018-01-09 11:26:25 +0800

    开通好友关系

---
+++ js/add_friends.js
@@ -0,0 +1,15 @@
+$(function(){
+	$(".bxg-submit").click(function(){
+		$(".radar-rotate").addClass("radar-ani");
+		$.ajax({
+			type:"get",
+			url:"",
+			success:function(){
+				
+			},
+			error:function(){
+				
+			}
+		});
+	})
+});
